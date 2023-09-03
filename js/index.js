8d2d24f8249db1066ef1e43d3db3ab6a3cb13cb2
lee0203-jun <conljm97@gmail.com>
2023-09-03 13:24:09 +0900

    배포

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(function(){
+
+  
+
+$('.vic ').hover(function(){
+
+
+$(this).addClass('on');
+
+});
+
+$('.vic').mouseleave(function(){
+
+
+  $(this).removeClass('on');
+
+
+});
+
+$('.box').hover(function(){
+
+$(this).addClass('on');
+
+
+});
+
+$('.box').mouseleave(function(){
+
+  $(this).removeClass('on');
+})
+
+
+
+
+
+});
